4aa6ed9172f0b649b9dd55df823dbfc428ca1091
Qian Pan <panqian.la@gmail.com>
2020-05-23 00:56:41 -0400

    Service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,9 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const Terserplugin = require('terser-webpack-plugin');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -46,5 +48,6 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({ filename: '[name].css' }),
         // new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW()
     ]
 }
